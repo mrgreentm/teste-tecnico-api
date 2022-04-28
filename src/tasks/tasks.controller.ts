a2f74ae254afeee36995b66b712a30d90c475b7a
João Jacinto <joaonetotfm@gmail.com>
2022-04-28 19:35:39 -0300

    feature: implementing-guards

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,31 +9,38 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksCreateDto } from './dtos/tasks-create.dto';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private service: TasksService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async findAll(): Promise<TasksInterface[]> {
     const tasks = await this.service.findAll();
     return tasks;
   }
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   async findOne(@Param('id') id: number): Promise<TasksInterface> {
     return await this.service.findOne(id);
   }
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() task: TasksCreateDto): Promise<TasksInterface> {
     return this.service.create(task);
   }
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async delete(@Param('id') id: number): Promise<void> {
     return await this.service.delete(id);
   }
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   async update(
     @Param('id') id: number,
     @Body() task: TasksUpdate,
